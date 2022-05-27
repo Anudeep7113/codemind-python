bb2cdf1b403aad023bbae55fb375b4155babbae7
Anudeep7113 <97575253+Anudeep7113@users.noreply.github.com>
2022-05-27 22:29:45 +0530

    x=int(input()) a=[] while(x>0):     r=x%10     a.append(r)     x=x//10 for i in range(len(a)-1,-1,-1):     if a[i]==6:         a[i]=9         break for i in range(len(a)-1,-1,-1):     print(a[i],end='')

---
+++ Maximum_69_Number.py
@@ -0,0 +1,13 @@
+x=int(input())
+a=[]
+while(x>0):
+    r=x%10
+    a.append(r)
+    x=x//10
+for i in range(len(a)-1,-1,-1):
+    if a[i]==6:
+        a[i]=9
+        break
+for i in range(len(a)-1,-1,-1):
+    print(a[i],end='')
+        
